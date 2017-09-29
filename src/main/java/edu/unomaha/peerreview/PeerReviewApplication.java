a2a45bde765fad6d598188c3de9e6367e27b05ca
Henry McNeil <vidmaster@gmail.com>
2017-09-28 19:39:27 -0500

    Initial maven application

---
+++ src/main/java/edu/unomaha/peerreview/PeerReviewApplication.java
@@ -0,0 +1,12 @@
+package edu.unomaha.peerreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerReviewApplication.class, args);
+	}
+}
