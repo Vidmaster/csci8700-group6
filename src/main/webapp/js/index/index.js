68f6c3e35d90c4b72d96098a741cb3f776cec56e
McNeil, Henry C <henry.mcneil@tdameritrade.com>
2017-11-27 16:52:30 -0600

    Janky implementation of different student/professor view based on login

--- src/main/webapp/js/index/index.js
+++ src/main/webapp/js/index/index.js
@@ -1,5 +1,12 @@
 angular.
-  module('prIndex', []).
-  component('prIndex', {
+  module('prIndex', [])
+  .component('prIndex', {
     templateUrl: '/js/index/index.template.html'
-});
+  })
+  .controller('index', function($scope, $rootScope, $http, $location, auth, $window) {
+	  var self = this;
+	  var student = auth.user.principal.userRole == 'STUDENT';
+	  $scope.student = student;
+	  console.log(auth.user.principal.userRole);
+	  console.log(student);
+  });
